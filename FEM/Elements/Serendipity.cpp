16aff1f052df4d8f53b9dde82bab09c485b3e0c6
Arturo Rodriguez <davidarturo9911@hotmail.com>
2022-11-04 10:07:26 -0500

    Serendipity shape functions and static PSI working!

--- FEM/Elements/Serendipity.cpp
+++ FEM/Elements/Serendipity.cpp
@@ -13,29 +13,20 @@ namespace FEM
 	std::vector<std::vector<double>> Serendipity::psis(std::vector<std::vector<double>> &z)
 	{
 		std::vector<std::vector<double>> res;
-		// int n_z = z.row(0).size();
-		// Eigen::MatrixXd res(this->n, n_z);
-		// Eigen::ArrayXd _z = z.row(0).array();
-		// Eigen::ArrayXd _n = z.row(1).array();
+		int nz = z[0].size();
+		for (int i = 0; i < nz; i++)
+		{
+			std::vector<double> row = {0.25 * (1.0 - z[0][i]) * (1.0 - z[1][i]) * (-1.0 - z[0][i] - z[1][i]),
+									   0.25 * (1.0 + z[0][i]) * (1.0 - z[1][i]) * (-1.0 + z[0][i] - z[1][i]),
+									   0.25 * (1.0 + z[0][i]) * (1.0 + z[1][i]) * (-1.0 + z[0][i] + z[1][i]),
+									   0.25 * (1.0 - z[0][i]) * (1.0 + z[1][i]) * (-1.0 - z[0][i] + z[1][i]),
+									   0.5 * (1.0 - z[0][i] * z[0][i]) * (1.0 - z[1][i]),
+									   0.5 * (1.0 + z[0][i]) * (1.0 - z[1][i] * z[1][i]),
+									   0.5 * (1.0 - z[0][i] * z[0][i]) * (1.0 + z[1][i]),
+									   0.5 * (1.0 - z[0][i]) * (1.0 - z[1][i] * z[1][i])};
+			res.push_back(row);
+		}
 
-		// /**
-		//  * 0.25*(1.0-z[0])*(1.0-z[1])*(-1.0-z[0]-z[1]),
-		// 	0.25*(1.0+z[0])*(1.0-z[1])*(-1.0+z[0]-z[1]),
-		// 	0.25*(1.0+z[0])*(1.0+z[1])*(-1.0+z[0]+z[1]),
-		// 	0.25*(1.0-z[0])*(1.0+z[1])*(-1.0-z[0]+z[1]),
-		// 	0.5*(1.0-z[0]**2.0)*(1.0-z[1]),
-		// 	0.5*(1.0+z[0])*(1.0-z[1]**2.0),
-		// 	0.5*(1.0-z[0]**2.0)*(1.0+z[1]),
-		// 	0.5*(1.0-z[0])*(1.0-z[1]**2.0)
-		//  * */
-		// res << (0.25 * (1.0 - _z) * (1.0 - _n) * (-1.0 - _z - _n)).matrix().transpose(),
-		// 	(0.25 * (1.0 + _z) * (1.0 - _n) * (-1.0 + _z - _n)).matrix().transpose(),
-		// 	(0.25 * (1.0 + _z) * (1.0 + _n) * (-1.0 + _z + _n)).matrix().transpose(),
-		// 	(0.25 * (1.0 - _z) * (1.0 + _n) * (-1.0 - _z + _n)).matrix().transpose(),
-		// 	(0.5 * (1.0 - _z * _z) * (1.0 - _n)).matrix().transpose(),
-		// 	(0.5 * (1.0 + _z) * (1.0 - _n * _n)).matrix().transpose(),
-		// 	(0.5 * (1.0 - _z * _z) * (1.0 + _n)).matrix().transpose(),
-		// 	(0.5 * (1.0 - _z) * (1.0 - _n * _n)).matrix().transpose();
 		return res;
 	}
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include "FEM.h"
+#include "Element.h"
 #include "FEM/Geometry/Geometry.h"
 #include <Eigen/Dense>
+
+void printMatrix(std::vector<std::vector<double>> x)
+{
+    for (int i = 0; i < x.size(); i++)
+    {
+        for (int j = 0; j < x[i].size(); j++)
+        {
+            std::cout << x[i][j] << ",";
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main()
 {
     FEM::Geometry *geo = new FEM::Geometry("C:/Users/david/Desktop/FEM++/resources/Beam_serendipity.json");
-    std::cout << *geo << std::endl;
+    std::vector<std::vector<double>> *x = (*(geo->elements[0])).givePsi();
+    printMatrix(*x);
+    std::cout << std::endl;
+    std::vector<std::vector<double>> *x2 = (*(geo->elements[1])).givePsi();
+    printMatrix(*x2);
+    // std::cout << *geo << std::endl;
 }
